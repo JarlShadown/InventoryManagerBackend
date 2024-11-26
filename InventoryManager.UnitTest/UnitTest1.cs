fc76894adb668c82c1642011148a0f7be823e73b
JarlShadown <miguel.payran.del.angel@outlook.com>
2024-11-25 21:37:58 -0800

    first commit

---
+++ InventoryManager.UnitTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace InventoryManager.UnitTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
