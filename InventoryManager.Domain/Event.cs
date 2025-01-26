f70672d5404b3c9992754078e7c6dca2bad6c567
JarlShadown <miguel.payran.del.angel@outlook.com>
2025-01-25 17:37:37 -0800

    create primary class for events

--- InventoryManager.Domain/Event.cs
+++ InventoryManager.Domain/Event.cs
@@ -1,6 +1,29 @@
 namespace InventoryManager.Domain;
 
-public class Event
+public class Event<T>
 {
-    
+    public int EventId { get; set; }
+    public required string AggregateId { get; set; }
+    public required string AggregateType { get; set; }
+    public required EventContent<T> AggregateVersion { get; set; }
+    public required DateTime TimeStamp { get; set; } = DateTime.UtcNow;
+    public required int Version { get; set; }
 }
+
+public class EventContent<T>(T especialContent)
+{
+    public required string Location { get; set; }
+    public string LicensePlate { get; set; } = string.Empty;
+    public required string Item { get; set; }
+    public required int Quantity { get; set; }
+    public T? ExtraParameters { get; set; }
+}
+//
+// CREATE TABLE events (
+//     id SERIAL PRIMARY KEY,
+//     aggregate_type TEXT NOT NULL,
+//     event_type TEXT NOT NULL,
+//     event_data JSONB NOT NULL,
+//     timestamp TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+//     version INT NOT NULL
+// );
